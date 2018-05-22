29042499516a9c09f7697e7ccf584982ba9cf5fc
ludoaubert <ludo.aubert@gmail.com>
2018-05-22 09:21:53 +0000

    ERR_INVALID_HTTP_RESPONSE
    Python server on port 8080 ERR_INVALID_HTTP_RESPONSE when accessed from Chrome

--- webserver.py
+++ webserver.py
@@ -100,5 +100,5 @@ while True:
 #Hello, World!
 #"""
     http_response = bytearray(json.dumps(data),'ascii')
-    client_connection.sendall(http_response)
+    client_connection.sendall(b'HTTP/1.0 200 OK\r\nContent-Length: %d\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n%s\r\n' % (len(http_response), http_response))
     client_connection.close()
