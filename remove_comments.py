872a55d222e08461c503140947f5d30a06782359
Ludovic Aubert <ludo.aubert@gmail.com>
2021-10-02 19:19:56 +0200

    python script remove comments

--- remove_comments.py
+++ remove_comments.py
@@ -1,8 +1,14 @@
+import re
 
 f=open("bombix-beta.cpp", 'r')
 buffer = f.read()
 f.close()
-buffer2 = return re.sub('//.*?\n|/\*.*?\*/', '', buffer, flags=re.S)
+buffer2 = re.sub('//.*?\n|/\*.*?\*/', '', buffer, flags=re.S)
+
+strings_to_replace = {'HORIZONTAL': 'H', 'VERTICAL': 'V', 'INCREASE': 'I', 'DECREASE': 'D'} 
+
+for key, value in strings_to_replace.items():
+    buffer2 = buffer2.replace(key, value)
 
 f=open("bombix-beta2.cpp", 'w')
 f.write(buffer2)
