5f9ff94d449c163991343e9a539c1b0a3ba1f039
Ludovic Aubert <ludo.aubert@gmail.com>
2021-12-10 11:29:17 +0100

    bug fix

--- SelectMetaData.sql
+++ SelectMetaData.sql
@@ -19,16 +19,7 @@ WITH cte_fk AS (
 		ON tab2.object_id = fkc.referenced_object_id
 	INNER JOIN sys.columns col2
 		ON col2.column_id = referenced_column_id AND col2.object_id = tab2.object_id
-) , cte_table_list AS (
-	SELECT table_name, ROW_NUMBER() OVER (ORDER BY table_name) AS rn
-	FROM INFORMATION_SCHEMA.TABLES
-	WHERE EXISTS (SELECT * FROM cte_fk WHERE TABLE_NAME IN ([table], [referenced_table]))
-) , cte_table_column_list AS (
-	SELECT c.table_name, c.column_name, tl.rn AS rn_table,
-		ROW_NUMBER() OVER (PARTITION BY c.table_name ORDER BY c.column_name) AS rn_column
-	FROM INFORMATION_SCHEMA.COLUMNS c
-	JOIN cte_table_list tl ON c.TABLE_NAME=tl.TABLE_NAME
-) , cte_pk AS (
+), cte_pk AS (
 	select schema_name(tab.schema_id) as [schema_name], 
 		pk.[name] as pk_name,
 		substring(column_names, 1, len(column_names)-1) as [columns],
@@ -46,6 +37,17 @@ WITH cte_fk AS (
 							and ic.index_id = pk.index_id
 								order by col.column_id
 								for xml path ('') ) D (column_names)
+) , cte_table_list AS (
+	SELECT table_name, ROW_NUMBER() OVER (ORDER BY table_name) AS rn
+	FROM INFORMATION_SCHEMA.TABLES
+	WHERE EXISTS (SELECT * FROM cte_fk WHERE TABLE_NAME IN ([table], [referenced_table]))
+) , cte_table_column_list AS (
+	SELECT c.table_name, c.column_name, tl.rn AS rn_table,
+		ROW_NUMBER() OVER (PARTITION BY c.table_name ORDER BY pk.pk_name DESC, fk.FK_NAME DESC, c.column_name) AS rn_column
+	FROM INFORMATION_SCHEMA.COLUMNS c
+	JOIN cte_table_list tl ON c.TABLE_NAME=tl.TABLE_NAME
+	LEFT JOIN cte_pk pk ON c.TABLE_NAME=pk.table_name AND c.COLUMN_NAME = pk.[columns]
+	LEFT JOIN cte_fk fk ON c.TABLE_NAME=fk.[table] AND c.COLUMN_NAME = fk.[column]
 ) , cte_fields AS (
 	SELECT tl.table_name AS title, (
 		SELECT tc.COLUMN_NAME AS [name], 
@@ -55,7 +57,7 @@ WITH cte_fk AS (
 		LEFT JOIN cte_pk pk ON tc.TABLE_NAME=pk.table_name AND tc.COLUMN_NAME = pk.[columns]
 		LEFT JOIN cte_fk fk ON tc.TABLE_NAME=fk.[table] AND tc.COLUMN_NAME = fk.[column]
 		WHERE tc.TABLE_NAME = tl.table_name
-		ORDER BY pk.pk_name DESC, fk.FK_NAME DESC, tc.COLUMN_NAME
+		ORDER BY tc.rn_column
 		FOR JSON PATH
 	) AS fields 
 	FROM cte_table_list tl
