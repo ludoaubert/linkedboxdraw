2e4fe8c01b510b3f89e1bf66aea84dd4ac15f826
Ludovic Aubert <ludo.aubert@gmail.com>
2022-03-22 21:00:18 +0100

    stay away from INFORMATION_SCHEMA (Aaron Bertrand)

--- SelectMetaData_SQLServer.sql
+++ SelectMetaData_SQLServer.sql
@@ -1,14 +1,25 @@
 
 WITH cte_fk AS (
-	SELECT rc.constraint_name AS FK_NAME,
-		rc.CONSTRAINT_SCHEMA AS [schema_name],
-		ccu.table_name AS [table],
-		ccu.column_name AS [column],
-		kcu.table_name AS [referenced_table],
-		kcu.column_name AS [referenced_column]
-	FROM INFORMATION_SCHEMA.CONSTRAINT_COLUMN_USAGE ccu
-	JOIN INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS rc ON ccu.CONSTRAINT_NAME = rc.CONSTRAINT_NAME 
-    JOIN INFORMATION_SCHEMA.KEY_COLUMN_USAGE kcu ON kcu.CONSTRAINT_NAME = rc.UNIQUE_CONSTRAINT_NAME  
+-- stay away from INFORMATION_SCHEMA on SQL Server (Aaron Bertrand)
+	SELECT  obj.name AS FK_NAME,
+		sch.name AS [schema_name],
+		tab1.name AS [table],
+		col1.name AS [column],
+		tab2.name AS [referenced_table],
+		col2.name AS [referenced_column]
+	FROM sys.foreign_key_columns fkc
+	INNER JOIN sys.objects obj
+		ON obj.object_id = fkc.constraint_object_id
+	INNER JOIN sys.tables tab1
+		ON tab1.object_id = fkc.parent_object_id
+	INNER JOIN sys.schemas sch
+		ON tab1.schema_id = sch.schema_id
+	INNER JOIN sys.columns col1
+		ON col1.column_id = parent_column_id AND col1.object_id = tab1.object_id
+	INNER JOIN sys.tables tab2
+		ON tab2.object_id = fkc.referenced_object_id
+	INNER JOIN sys.columns col2
+		ON col2.column_id = referenced_column_id AND col2.object_id = tab2.object_id
 ), cte_pk AS (
 	SELECT Tab.CONSTRAINT_SCHEMA AS [schema_name],
 		Tab.CONSTRAINT_NAME AS [pk_name],
