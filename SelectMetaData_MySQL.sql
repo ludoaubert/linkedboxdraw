ecb130f29f406d339d062457a72a23412be901e1
Ludovic Aubert <ludo.aubert@gmail.com>
2022-03-26 23:14:22 +0100

    Add files via upload

--- SelectMetaData_MySQL.sql
+++ SelectMetaData_MySQL.sql
@@ -49,48 +49,20 @@ where tco.constraint_type = 'PRIMARY KEY'
 		ORDER BY tc.rn_column
 	) AS fields 
 	FROM cte_table_list tl
-),  cte_rectangle_constants AS (
-	SELECT 7 AS MONOSPACE_FONT_PIXEL_WIDTH,
-		16 AS CHAR_RECT_HEIGHT,	-- in reality 14,8 + 1 + 1 (top and bottom padding) = 16,8
-		200 AS RECTANGLE_BOTTOM_CAP
-), cte_rectangles_staging AS (
-	SELECT tc.TABLE_NAME,
-		CASE 
-			WHEN pk.pk_name IS NOT NULL AND fk.FK_NAME IS NOT NULL
-				THEN (LENGTH('PK FK ') + LENGTH(column_name)) * MONOSPACE_FONT_PIXEL_WIDTH
-			WHEN pk.pk_name IS NOT NULL AND fk.FK_NAME IS NULL
-				THEN (LENGTH('PK ') + LENGTH(column_name)) * MONOSPACE_FONT_PIXEL_WIDTH
-			WHEN pk.pk_name IS NULL AND fk.FK_NAME IS NOT NULL
-				THEN (LENGTH('FK ') + LENGTH(column_name)) * MONOSPACE_FONT_PIXEL_WIDTH
-			WHEN pk.pk_name IS NULL AND fk.FK_NAME IS NULL
-				THEN LENGTH(column_name) * MONOSPACE_FONT_PIXEL_WIDTH
-		END AS column_width
-	FROM cte_table_column_list tc
-	LEFT JOIN cte_pk pk ON tc.TABLE_NAME=pk.table_name AND tc.COLUMN_NAME = pk.columns
-	LEFT JOIN cte_fk fk ON tc.TABLE_NAME=fk.table AND tc.COLUMN_NAME = fk.column
-	CROSS JOIN cte_rectangle_constants
-
-	UNION ALL
-
-	SELECT table_name, 2*4 + LENGTH(table_name) * MONOSPACE_FONT_PIXEL_WIDTH
-	FROM cte_table_list 
-	CROSS JOIN cte_rectangle_constants
-), cte_rectangles AS (
-	SELECT tl.TABLE_NAME,
-		0 AS "left", 
-		(
-			SELECT MAX(column_width)
-			FROM cte_rectangles_staging rs 
-			WHERE rs.table_name = tl.table_name
-		) AS "right",
-		0 AS "top",
-		(
-			SELECT 8 + CHAR_RECT_HEIGHT * (COUNT(*) + 1)
-			FROM cte_table_column_list tcl 
-			WHERE tcl.table_name = tl.table_name
-		) AS "bottom"
-	FROM cte_table_list tl
-	CROSS JOIN cte_rectangle_constants rc
+),  cte_links AS (
+		SELECT tcl_from.rn_table - 1 AS [from],	-- zero based
+			tcl_from.rn_column - 1 AS fromField, -- zero based
+			NULL AS fromCardinality,
+			tcl_to.rn_table - 1 AS [to],	-- zero based
+			tcl_to.rn_column - 1 AS toField,-- zero based
+			NULL AS toCardinality
+		FROM cte_fk fk
+		JOIN cte_table_column_list tcl_from ON fk.[table]=tcl_from.TABLE_NAME AND fk.[column]=tcl_from.COLUMN_NAME
+		JOIN cte_table_column_list tcl_to ON fk.referenced_table=tcl_to.TABLE_NAME AND fk.referenced_column=tcl_to.COLUMN_NAME
+),	cte_tr2 AS (
+		SELECT l1.[from], l2.[to]
+		FROM cte_links l1
+		JOIN cte_links l2 ON l1.[to] = l2.[from]
 ), cte_diagdata AS (
 	SELECT 'DilabProject' AS documentTitle,
 	(
@@ -102,31 +74,20 @@ where tco.constraint_type = 'PRIMARY KEY'
 		ORDER BY title
 	) AS boxes,
 	(
-		SELECT CONCAT('[',GROUP_CONCAT(JSON_OBJECT("from", tcl_from.rn_table - 1,	-- zero based
-			"fromField",tcl_from.rn_column - 1, -- zero based
+		SELECT CONCAT('[',GROUP_CONCAT(JSON_OBJECT("from", l.[from],
+			"fromField",l.[fromField],
 			"fromCardinality", NULL, 
-			"to", tcl_to.rn_table - 1,	-- zero based
-			"toField", tcl_to.rn_column - 1,-- zero based
-			"toCardinality", NULL)),']')
-		FROM cte_fk fk
-		JOIN cte_table_column_list tcl_from ON fk.table=tcl_from.TABLE_NAME AND fk.column=tcl_from.COLUMN_NAME
-		JOIN cte_table_column_list tcl_to ON fk.referenced_table=tcl_to.TABLE_NAME AND fk.referenced_column=tcl_to.COLUMN_NAME
+			"to", l.[to],
+			"toField", l.[toField],
+			"toCardinality", NULL,
+			"Category",CASE
+						WHEN EXISTS(SELECT * FROM cte_tr2 WHERE cte_tr2.[from]=l.[from] AND cte_tr2.[to]=l.[to])
+							THEN 'TR2'
+						ELSE ''
+						END
+			)),']')
+		FROM cte_links l
 	) AS links,
-	(
-		SELECT CONCAT('[',GROUP_CONCAT(JSON_OBJECT(
-        "left", cte_rectangles.left,
-        "right", cte_rectangles.right,
-        "top", cte_rectangles.top,
-        "bottom",
-			CASE 
-				WHEN cte_rectangles.bottom > RECTANGLE_BOTTOM_CAP 
-					THEN RECTANGLE_BOTTOM_CAP 
-				ELSE cte_rectangles.bottom 
-			END)),']')
-		FROM cte_rectangles 
-		CROSS JOIN cte_rectangle_constants
-		ORDER BY TABLE_NAME
-	) AS rectangles,
 	json_query('{ "key1" : [] }','$.key1') AS "values",
 	json_query('{ "key1" : [] }','$.key1') AS "boxComments",
 	json_query('{ "key1" : [] }','$.key1') AS "fieldComments",
@@ -137,7 +98,6 @@ GROUP_CONCAT(JSON_OBJECT(
 'documentTitle', documentTitle,
 "boxes",boxes,
 "links",links,
-"rectangles",rectangles,
 "values", cte_diagdata.values,
 "boxComments",boxComments,
 "fieldComments",fieldComments,
