77a04f9d79d0b84a168084930cc7c29e2f9d33f3
Ludovic Aubert <ludo.aubert@gmail.com>
2022-05-13 08:49:24 +0200

    Add files via upload

--- table_edit.js
+++ table_edit.js
@@ -563,14 +563,14 @@ function dropFieldComment()
 
 function updateFieldComment()
 {
-	const currentFieldCommentIndex = mydata.boxComments.findIndex(({box, field, comment}) => box == boxCombo.value && field == fieldCombo.value);
+	const currentFieldCommentIndex = mydata.fieldComments.findIndex(({box, field, comment}) => box == boxCombo.value && field == fieldCombo.value);
 	
 	const fieldComment = {box: boxCombo.value, field: fieldCombo.value, comment: fieldCommentTextArea.value};
 	
 	if (currentFieldCommentIndex != -1)
-		mydata.boxComments[ currentBoxCommentIndex ] = fieldComment;
+		mydata.fieldComments[ currentBoxCommentIndex ] = fieldComment;
 	else
-		mydata.boxComments.push(fieldComment);
+		mydata.fieldComments.push(fieldComment);
 	
 	displayCurrent();
 }
