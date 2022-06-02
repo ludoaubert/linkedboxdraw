13d68bdd7c476d1b860b1b6c19c36e491bb99ca8
Ludovic Aubert <ludo.aubert@gmail.com>
2022-06-02 13:47:42 +0200

    collapsibles the easy way

--- collapsible.js
+++ collapsible.js
@@ -1,18 +1,13 @@
 
 
-function setCollapsibleHandler()
-{
-	var coll = document.getElementsByClassName("collapsible");
-
-	for (i = 0; i < coll.length; i++) {
-		coll[i].addEventListener("click", function() {
-			this.classList.toggle("active");
-			var content = this.nextElementSibling;
-			if (content.style.display === "block") {
-			  content.style.display = "none";
-			} else {
-			  content.style.display = "block";
-			}
-		});
+function switchCollapsible() {
+	
+	this.classList.toggle("active");
+	var content = this.nextElementSibling;
+	if (content.style.display === "block") {
+		content.style.display = "none";
+	} 
+	else {
+		content.style.display = "block";
 	}
 }
