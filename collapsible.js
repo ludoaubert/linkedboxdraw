c2114c027142e3aab31145d09b5d55d70b1c3514
Ludovic Aubert <ludo.aubert@gmail.com>
2022-06-09 10:25:48 +0200

    Add files via upload

--- collapsible.js
+++ collapsible.js
@@ -4,6 +4,10 @@ function switchCollapsible(element) {
 	
 	element.classList.toggle("active");
 	var content = element.nextElementSibling;
+	
+	const cssObj = window.getComputedStyle(content);
+	let display = cssObj.getPropertyValue("display");
+	
 	if (content.style.display === "block") {
 		content.style.display = "none";
 	} 
