1defba0eb3c02993009ec4289ecbdbaa39e6736c
edoudabear <edou.aubert@gmail.com>
2022-06-12 13:24:17 +0200

    titi

--- latuile.cpp
+++ latuile.cpp
@@ -36,7 +36,7 @@ void test()
     TestFunctionTimer ft("test_latuile");
 
     struct TestOutputContext{MyRect frame; vector<TranslatedBox> translatedBoxes;};
-    struct TestContext{int testid; vector<MyRect> input_rectangles; vector<Edge> edges; vector<TestOutputContext> expected_contexts;};
+    struct TestContext{int testid; string title; vector<MyRect> input_rectangles; vector<Edge> edges; vector<TestOutputContext> expected_contexts;};
 
 
 /*
@@ -85,52 +85,53 @@ void test()
     const vector<TestContext> test_contexts = {
 {
 	.testid=1,
+	.title="Verlan",
 	.input_rectangles={
-{.m_left=0, .m_right=141, .m_top=0, .m_bottom=40, .no_sequence=0},
-{.m_left=0, .m_right=162, .m_top=0, .m_bottom=56, .no_sequence=1 },
-{.m_left=0, .m_right=64, .m_top=0, .m_bottom=104, .no_sequence=2 },
-{.m_left=0, .m_right=120, .m_top=0, .m_bottom=120, .no_sequence=3 },
-{.m_left=0, .m_right=141, .m_top=0, .m_bottom=56, .no_sequence=4 },
-{.m_left=0, .m_right=267, .m_top=0, .m_bottom=72, .no_sequence=5 },
-{.m_left=0, .m_right=71, .m_top=0, .m_bottom=136, .no_sequence=6 },
-{.m_left=0, .m_right=78, .m_top=0, .m_bottom=72, .no_sequence=7 },
-{.m_left=0, .m_right=211, .m_top=0, .m_bottom=72, .no_sequence=8 },
-{.m_left=0, .m_right=133, .m_top=0, .m_bottom=88, .no_sequence=9 },
-{.m_left=0, .m_right=106, .m_top=0, .m_bottom=104, .no_sequence=10 },
-{.m_left=0, .m_right=105, .m_top=0, .m_bottom=120, .no_sequence=11 },
-{.m_left=0, .m_right=57, .m_top=0, .m_bottom=56, .no_sequence=12 },
-{.m_left=0, .m_right=63, .m_top=0, .m_bottom=72, .no_sequence=13 },
-{.m_left=0, .m_right=154, .m_top=0, .m_bottom=88, .no_sequence=14 },
-{.m_left=0, .m_right=98, .m_top=0, .m_bottom=56, .no_sequence=15 },
-{.m_left=0, .m_right=112, .m_top=0, .m_bottom=56, .no_sequence=16 },
-{.m_left=0, .m_right=112, .m_top=0, .m_bottom=72, .no_sequence=17 },
-{.m_left=0, .m_right=50, .m_top=0, .m_bottom=56, .no_sequence=18 },
-{.m_left=0, .m_right=196, .m_top=0, .m_bottom=120, .no_sequence=19 }
+		{.m_left=0, .m_right=141, .m_top=0, .m_bottom=40, .no_sequence=0},
+		{.m_left=0, .m_right=162, .m_top=0, .m_bottom=56, .no_sequence=1 },
+		{.m_left=0, .m_right=64, .m_top=0, .m_bottom=104, .no_sequence=2 },
+		{.m_left=0, .m_right=120, .m_top=0, .m_bottom=120, .no_sequence=3 },
+		{.m_left=0, .m_right=141, .m_top=0, .m_bottom=56, .no_sequence=4 },
+		{.m_left=0, .m_right=267, .m_top=0, .m_bottom=72, .no_sequence=5 },
+		{.m_left=0, .m_right=71, .m_top=0, .m_bottom=136, .no_sequence=6 },
+		{.m_left=0, .m_right=78, .m_top=0, .m_bottom=72, .no_sequence=7 },
+		{.m_left=0, .m_right=211, .m_top=0, .m_bottom=72, .no_sequence=8 },
+		{.m_left=0, .m_right=133, .m_top=0, .m_bottom=88, .no_sequence=9 },
+		{.m_left=0, .m_right=106, .m_top=0, .m_bottom=104, .no_sequence=10 },
+		{.m_left=0, .m_right=105, .m_top=0, .m_bottom=120, .no_sequence=11 },
+		{.m_left=0, .m_right=57, .m_top=0, .m_bottom=56, .no_sequence=12 },
+		{.m_left=0, .m_right=63, .m_top=0, .m_bottom=72, .no_sequence=13 },
+		{.m_left=0, .m_right=154, .m_top=0, .m_bottom=88, .no_sequence=14 },
+		{.m_left=0, .m_right=98, .m_top=0, .m_bottom=56, .no_sequence=15 },
+		{.m_left=0, .m_right=112, .m_top=0, .m_bottom=56, .no_sequence=16 },
+		{.m_left=0, .m_right=112, .m_top=0, .m_bottom=72, .no_sequence=17 },
+		{.m_left=0, .m_right=50, .m_top=0, .m_bottom=56, .no_sequence=18 },
+		{.m_left=0, .m_right=196, .m_top=0, .m_bottom=120, .no_sequence=19 }
  	},
 	.edges={
- {.from=1,  .to=14 },
- {.from=2,  .to=14 },
- {.from=2,  .to=5 },
- {.from=3,  .to=4 },
- {.from=5,  .to=17 },
- {.from=6,  .to=14 },
- {.from=6,  .to=5 },
- {.from=7,  .to=0 },
- {.from=7,  .to=16 },
- {.from=8,  .to=9 },
- {.from=9,  .to=15 },
- {.from=10,  .to=18 },
- {.from=10,  .to=16 },
- {.from=10,  .to=9 },
- {.from=11,  .to=13 },
- {.from=12,  .to=14 },
- {.from=14,  .to=7 },
- {.from=16,  .to=3 },
- {.from=17,  .to=7 },
- {.from=19,  .to=10 },
- {.from=19,  .to=7 },
- {.from=19,  .to=8 },
- {.from=19,  .to=11 }
+ 		{.from=1,  .to=14 },
+ 		{.from=2,  .to=14 },
+ 		{.from=2,  .to=5 },
+ 		{.from=3,  .to=4 },
+ 		{.from=5,  .to=17 },
+ 		{.from=6,  .to=14 },
+ 		{.from=6,  .to=5 },
+ 		{.from=7,  .to=0 },
+ 		{.from=7,  .to=16 },
+ 		{.from=8,  .to=9 },
+ 		{.from=9,  .to=15 },
+ 		{.from=10,  .to=18 },
+ 		{.from=10,  .to=16 },
+ 		{.from=10,  .to=9 },
+ 		{.from=11,  .to=13 },
+ 		{.from=12,  .to=14 },
+ 		{.from=14,  .to=7 },
+ 		{.from=16,  .to=3 },
+ 		{.from=17,  .to=7 },
+ 		{.from=19,  .to=10 },
+ 		{.from=19,  .to=7 },
+ 		{.from=19,  .to=8 },
+ 		{.from=19,  .to=11 }
    	},
 
 	.expected_contexts={
@@ -160,10 +161,69 @@ void test()
 	        }
             }
 	}
+},
+{
+	.testid=2,
+	.title="LOLA",
+	.input_rectangles={
+		{.m_left=0,.m_right=162,.m_top=0,.m_bottom=104, .no_sequence=0},//8-0
+		{.m_left=0,.m_right=182,.m_top=0,.m_bottom=72, .no_sequence=1},//9-1
+		{.m_left=0,.m_right=105,.m_top=0,.m_bottom=72, .no_sequence=2},//10-2
+		{.m_left=0,.m_right=126,.m_top=0,.m_bottom=152, .no_sequence=3},//21-3
+		{.m_left=0,.m_right=126,.m_top=0,.m_bottom=88, .no_sequence=4},//24-4
+		{.m_left=0,.m_right=147,.m_top=0,.m_bottom=120, .no_sequence=5},//25-5
+		{.m_left=0,.m_right=140,.m_top=0,.m_bottom=120, .no_sequence=6},//26-6
+		{.m_left=0,.m_right=168,.m_top=0,.m_bottom=136, .no_sequence=7},//27-7
+		{.m_left=0,.m_right=168,.m_top=0,.m_bottom=120, .no_sequence=8},//28-8
+		{.m_left=0,.m_right=147,.m_top=0,.m_bottom=104, .no_sequence=9},//30-9
+		{.m_left=0,.m_right=133,.m_top=0,.m_bottom=120, .no_sequence=10},//32-10
+		{.m_left=0,.m_right=147,.m_top=0,.m_bottom=168, .no_sequence=11},//44-11
+		{.m_left=0,.m_right=140,.m_top=0,.m_bottom=88, .no_sequence=12},//48-12
+		{.m_left=0,.m_right=155,.m_top=0,.m_bottom=120, .no_sequence=13},//52-13
+		{.m_left=0,.m_right=175,.m_top=0,.m_bottom=136, .no_sequence=14}//53-14
+	},
+        .edges={
+		{.from=13,.to=12},
+		{.from=13,.to=14},
+		{.from=4,.to=7},
+		{.from=3,.to=4},
+		{.from=1,.to=7},
+		{.from=5,.to=7},
+		{.from=7,.to=11},
+		{.from=6,.to=7},
+		{.from=10,.to=7},
+		{.from=9,.to=7},
+		{.from=2,.to=7},
+		{.from=8,.to=7},
+		{.from=0,.to=3},
+		{.from=12,.to=6}
+	},
+        .expected_contexts={
+	    {
+		.frame={.m_left=0,.m_right=884,.m_top=0,.m_bottom=552},
+		.translatedBoxes={
+			{.id=0,.translation={.x=396,.y=10}},
+			{.id=1,.translation={.x=320,.y=330}},
+			{.id=2,.translation={.x=453,.y=218}},
+			{.id=3,.translation={.x=598,.y=10}},
+			{.id=4,.translation={.x=598,.y=202}},
+			{.id=5,.translation={.x=750,.y=346}},
+			{.id=6,.translation={.x=273,.y=154}},
+			{.id=7,.translation={.x=542,.y=330}},
+			{.id=8,.translation={.x=335,.y=506}},
+			{.id=9,.translation={.x=556,.y=506}},
+			{.id=10,.translation={.x=764,.y=186}},
+			{.id=11,.translation={.x=743,.y=506}},
+			{.id=12,.translation={.x=93,.y=153}},
+			{.id=13,.translation={.x=10,.y=281}},
+			{.id=14,.translation={.x=11,.y=441}}
+                },
+            }
+	}
 }
-    };
+};
 
-	for (const auto& [testid, input_rectangles, edges, expected_contexts] : test_contexts)
+	for (const auto& [testid, title, input_rectangles, edges, expected_contexts] : test_contexts)
 	{
 		vector<MyRect> rectangles = input_rectangles;
 		int n = rectangles.size() ;
@@ -194,7 +254,7 @@ void test()
  				translatedBoxes.push_back({rec.no_sequence, {rec.m_left, rec.m_top}});
         		}
 			bool bOK = expected_contexts[i].translatedBoxes == translatedBoxes;
-        		printf("%s\n", bOK ? "OK" : "KO");
+        		printf("test latuile %s %s\n", title.c_str(), bOK ? "OK" : "KO");
 			(bOK ? nbOK : nbKO)++;
 
         		vector<MyRect> expected_rectangles = input_rectangles;
