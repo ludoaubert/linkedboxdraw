2c630d4b281b8f13f0b170fbab53be8fd1148d39
edoudabear <edou.aubert@gmail.com>
2022-07-31 13:58:11 +0200

    titi

--- rect_union.cpp
+++ rect_union.cpp
@@ -1,8 +1,15 @@
 #include <algorithm>
+#include <ranges>
+#include <cmath>
+using namespace std;
 
 
 int main(int argc, char* argv[])
 {
+	int n=18;	//18 rectangles
+//generate all subsets containing p=3 rectangles
+	int p=3;
+	views::iota(0, pow(n,p));
 
 	return 0;
 }
