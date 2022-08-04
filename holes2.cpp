39c4e4df27b3633e29a951a8e96dd09a09cfab31
Ludovic Aubert <ludo.aubert@gmail.com>
2022-08-04 11:11:17 +0200

    Add files via upload

--- holes2.cpp
+++ holes2.cpp
@@ -478,9 +478,9 @@ int main()
 				}
 
 			//ensuite on mappe les liens de i et on regarde si ils figurent bien dans les liens de j
-			// en ne gardant que les liens de i {e dont e.j a deja ete mappé}
+			// en ne gardant que les liens de i {e dont e.j a deja ete mappé ou e.j que l'on ne peut deplacer}
 				auto rg3 = span(&logical_edges[start_pos1], end_pos1 - start_pos1) |
-					views::filter([&](const LogicalEdge& e){return mapping[e.to]!=j;}) |
+					views::filter([&](const LogicalEdge& e){return mapping[e.to]!=e.to || connected_component[e.to] == cmax;}) |
 					views::transform([&](const LogicalEdge& e){return TopologicalEdge{.from=mapping[e.from], .to=mapping[e.to], .distance=0};});
 
 				auto rg4 = span(&topological_edges[start_pos2], end_pos2 - start_pos2);
