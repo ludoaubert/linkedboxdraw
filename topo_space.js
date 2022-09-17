952219f45748fe62d96b4dcf49fe591338514f88
edoudabear <edou.aubert@gmail.com>
2022-09-17 17:16:12 +0200

    titi

--- topo_space.js
+++ topo_space.js
@@ -88,13 +88,13 @@ function print_html()
 			topological_edges.filter(({from, to}) => from==index)
 					.map(({from, to}, line) => `<text x="30" y="${14*(line+1)}" class="topological_contact">r-${to}</text>`),
 			`</g>`]),
-		holes.holes.map(({rec}, index) =>
+		holes.holes.map(({rec, ri}, index) =>
 			[`<g id="g-h-${index}" transform="translate(${rec.m_left} ${rec.m_top})">`,
 			`<rect x="0" y="0" width="${rec.m_right-rec.m_left}" height="${rec.m_bottom-rec.m_top}" class="hole" />`,
 			`<text x="0" y="0" fill="black">hole-${index}</text>`,
 			holes.topological_contact.filter(({hi, rj}) => hi==index)
 						.map(({hi, rj}, line) => `<text x="8" y="${14*(line+1)}" fill="black">r-${rj}</text>`),
-			`<text x="30" y="14" fill="black">ri=${hole.ri}</text>`,
+			`<text x="30" y="14" fill="black">ri=${ri}</text>`,
 			`</g>`]),
 		"</svg>"].flat()
 			.join('\n');
