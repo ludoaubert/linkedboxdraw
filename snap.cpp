9213d814bf1e2791cdc0652c487c2b4cbfd3e6af
Ludovic Aubert <ludo.aubert@gmail.com>
2022-09-21 17:10:37 +0200

    Add files via upload

--- snap.cpp
+++ snap.cpp
@@ -1,13 +1,14 @@
 
 
-
+/*
 void dim_swap(MyRect& r)
 {
 	swap(r.m_left, r.m_top);
 	swap(r.m_right, r.m_bottom);
 }
+*/
 
-enum RectTrim
+enum TrimAlgo
 {
 	SPLIT,
 	CHIP,
@@ -15,12 +16,49 @@ enum RectTrim
 	CORNER
 };
 
+enum TrimMirrorDirection
+{
+	HORIZONTAL_MIRROR,
+	VERTICAL_MIRROR,
+	TILTED_MIRROR
+};
+
+const TrimMirrorDirection trim_mirror_directions[3] = {HORIZONTAL_MIRROR,VERTICAL_MIRROR,TILTED_MIRROR} ;
+
+const MirroringState TrimMirrorSates[2] = {ACTIVE, IDLE};
+
+const Mirror mirrors[NR_MIRRORING_OPTIONS][3]={
+	{
+		{.mirroring_state=IDLE, .mirroring_direction=HORIZONTAL_MIRROR},
+		{.mirroring_state=IDLE, .mirroring_direction=VERTICAL_MIRROR}
+	},
+	{
+		{.mirroring_state=IDLE, .mirroring_direction=EAST_WEST},
+		{.mirroring_state=ACTIVE, .mirroring_direction=NORTH_SOUTH}
+	},
+	{
+		{.mirroring_state=ACTIVE, .mirroring_direction=EAST_WEST},
+		{.mirroring_state=IDLE, .mirroring_direction=NORTH_SOUTH}
+	},
+	{
+		{.mirroring_state=ACTIVE, .mirroring_direction=EAST_WEST},
+		{.mirroring_state=ACTIVE, .mirroring_direction=NORTH_SOUTH}
+	}
+};
+
+const char* MirroringStrings[NR_MIRRORING_OPTIONS]={
+	"IDLE,IDLE",
+	"IDLE,ACTIVE",
+	"ACTIVE,IDLE",
+	"ACTIVE,ACTIVE"
+};
+
 const int NR_TRIM_CONFIG=4;
 const int NR_MIRRORING=3;
 
 struct TrimSelector
 {
-	int rect_trim;
+	int trim_algo;
 	int mirroring;
 };
 
@@ -112,5 +150,25 @@ void corner(const MyRect& r, const MyRect& by, vector<MyRect>& rects)
 			{.m_left=r.m_left, .m_right=r.m_right, .m_top=r.m_top, .m_bottom=by.m_top},
 			{.m_left=by.m_right, .m_right=r.m_right, .m_top=r.m_top, .m_bottom=r.m_bottom}
 		};
+	}
+}
+
+
+void apply_trim(TrimAlgo trim_algo, const MyRect& r, const MyRect& by, vector<MyRect>& rects)
+{
+	switch (trim_algo)
+	{
+	case SPLIT:
+		split(r, by, rects);
+		break;
+	case CHIP:
+		chip(r, by, rects);
+		break;
+	case TRIM:
+		trim(r, by, rects);
+		break;
+	case CORNER:
+		corner(r, by, rects);
+		break;
 	}
 }
