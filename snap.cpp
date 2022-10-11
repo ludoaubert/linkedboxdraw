3bfc946327ffde3aa8831950ef3a47c63d7478fa
Ludovic Aubert <ludo.aubert@gmail.com>
2022-10-11 11:18:56 +0200

    Add files via upload

--- snap.cpp
+++ snap.cpp
@@ -4,13 +4,34 @@
 
 	vector<MyRect> rectangles = input_rectangles;
 
-        const int n = rectangles.size();
+	const int n = rectangles.size();
 
 	for (int id=0; ; id++)
 	{
-		const int frame_left = min(rectangles | views::transform(&MyRect::m_left));
+		bitset<30> partition(n,0);
+		
+		auto rec_select_partition=[&](int ri, auto&& rec_select_partition){
+			//TODO: avoid naming adj_list ?
+			span adj_list = ranges::equal_range(rect_links, ri, {}, &RectLink::i);
+			auto rg = adj_list | views::filter([&](const RectLink& rl){return rectangles[ri][maxCompactRectDim] == rectangles[rl.j][minCompactRectDim];})
+			ranges::for_each(rg, [&](const RectLink& rl){
+					partition[rl.j]=1;
+					rec_select_partition(rl.j, rec_select_partition);
+			});
 
-		auto rg = rect_links | views::filter([&](const RectLink& e){return rectangles[e.i].m_left==frame_left;})
+			}
+		};
+		
+		const int frame_left = min(rectangles | views::transform(&MyRect::m_left));
+		
+		auto rg = views::iota(0,n) | views::filter([&](int i){return rectangles[i].m_left==frame_left;});
+		
+		for (int ri : rg)
+		{
+			rec_select_partition(ri, rec_select_partition);
+		}
+
+		auto rg = rect_links | views::filter([&](const RectLink& e){return partition[e.i] != partition[e.j];})
 				| views::transform([&](const RectLink& e){return rectangles[e.j].m_left-rectangles[e.i].m_right;}) ;
 
 		int tr = min(rg);
