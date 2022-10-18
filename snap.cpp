914b4cb760fad1a7d666f6f083be1ddb4ff2830a
Ludovic Aubert <ludo.aubert@gmail.com>
2022-10-18 12:21:03 +0200

    Add files via upload

--- snap.cpp
+++ snap.cpp
@@ -1,259 +1,74 @@
-#include <vector>
-#include <algorithm>
-#include <numeric>
-#include <ranges>
-#include <bitset>
-#include <stdio.h>
-#include "MyRect.h"
-using namespace std;
-
-struct LogicalEdge {
-	int from;
-	int to;
-
-	friend auto operator<=>(const LogicalEdge&, const LogicalEdge&) = default;
-};
-
-struct RectLink{
-	int i, j, TOP, BOTTOM;
-};
-
-
-struct TranslationRangeItem
 {
-	int id;
-	int ri;
-	MyPoint tr;
-
-	friend bool operator==(const TranslationRangeItem&, const TranslationRangeItem&) = default;
-};
-
-const vector<MyRect> input_rectangles = {
-	{m_left: 328, m_right: 530, m_top: 10, m_bottom: 154},
-	{m_left: 252, m_right: 474, m_top: 490, m_bottom: 601},
-	{m_left: 385, m_right: 530, m_top: 218, m_bottom: 330},
-	{m_left: 530, m_right: 696, m_top: 10, m_bottom: 202},
-	{m_left: 530, m_right: 696, m_top: 202, m_bottom: 330},
-	{m_left: 682, m_right: 869, m_top: 346, m_bottom: 506},
-	{m_left: 267, m_right: 447, m_top: 601, m_bottom: 761},
-	{m_left: 474, m_right: 682, m_top: 330, m_bottom: 506},
-	{m_left: 266, m_right: 474, m_top: 330, m_bottom: 490},
-	{m_left: 488, m_right: 675, m_top: 506, m_bottom: 650},
-	{m_left: 744, m_right: 917, m_top: 186, m_bottom: 346},
-	{m_left: 675, m_right: 862, m_top: 506, m_bottom: 714},
-	{m_left: 25, m_right: 205, m_top: 153, m_bottom: 281},
-	{m_left: 10, m_right: 205, m_top: 281, m_bottom: 441},
-	{m_left: 37, m_right: 252, m_top: 441, m_bottom: 617}
-} ;
-
-//bi directional edges
-const vector<LogicalEdge> logical_edges = {
-        {.from=0,.to=3},
-        {.from=1,.to=7},
-        {.from=2,.to=7},
-        {.from=3,.to=0},
-        {.from=3,.to=4},
-        {.from=4,.to=3},
-        {.from=4,.to=7},
-        {.from=5,.to=7},
-        {.from=6,.to=7},
-        {.from=6,.to=12},
-        {.from=7,.to=1},
-        {.from=7,.to=2},
-        {.from=7,.to=4},
-        {.from=7,.to=5},
-        {.from=7,.to=6},
-        {.from=7,.to=8},
-        {.from=7,.to=9},
-        {.from=7,.to=10},
-        {.from=7,.to=11},
-        {.from=8,.to=7},
-        {.from=9,.to=7},
-        {.from=10,.to=7},
-        {.from=11,.to=7},
-        {.from=12,.to=6},
-        {.from=12,.to=13},
-        {.from=13,.to=12},
-        {.from=13,.to=14},
-        {.from=14,.to=13}
-};
-
-const vector<RectLink> rect_links={
-	{.i=0, .j=3, .TOP=10, .BOTTOM=154},
-	{.i=1, .j=7, .TOP=490, .BOTTOM=506},
-	{.i=1, .j=9, .TOP=506, .BOTTOM=601},
-	{.i=2, .j=4, .TOP=218, .BOTTOM=330},
-	{.i=3, .j=10, .TOP=186, .BOTTOM=202},
-	{.i=4, .j=10, .TOP=202, .BOTTOM=330},
-	{.i=6, .j=9, .TOP=601, .BOTTOM=650},
-	{.i=6, .j=11, .TOP=650, .BOTTOM=714},
-	{.i=7, .j=5, .TOP=346, .BOTTOM=506},
-	{.i=7, .j=10, .TOP=330, .BOTTOM=346},
-	{.i=8, .j=7, .TOP=330, .BOTTOM=490},
-	{.i=9, .j=11, .TOP=506, .BOTTOM=650},
-	{.i=12, .j=0, .TOP=153, .BOTTOM=154},
-	{.i=12, .j=2, .TOP=218, .BOTTOM=281},
-	{.i=12, .j=3, .TOP=154, .BOTTOM=202},
-	{.i=12, .j=4, .TOP=202, .BOTTOM=218},
-	{.i=13, .j=2, .TOP=281, .BOTTOM=330},
-	{.i=13, .j=8, .TOP=330, .BOTTOM=441},
-	{.i=14, .j=1, .TOP=490, .BOTTOM=601},
-	{.i=14, .j=6, .TOP=601, .BOTTOM=617},
-	{.i=14, .j=8, .TOP=441, .BOTTOM=490}
-};
-
-
-void compact(Direction update_direction, const vector<RectLink>& rect_links, const vector<LogicalEdge>& logical_edges, vector<MyRect>& rectangles)
-{
-	auto [minCompactRectDim, maxCompactRectDim] = rectDimRanges[update_direction];  //{LEFT, RIGHT} or {TOP, BOTTOM}
-
-	const vector<MyRect> rectangles_ = rectangles ;
-
-	int n = rectangles.size();
-
-        vector<vector<TranslationRangeItem> > vv(10);
-	int id=0;
-
-	auto next=[&](const vector<TranslationRangeItem>& prev)->vector<TranslationRangeItem>
-	{
-		vector<MyRect> rectangles = rectangles_;
-
-	        id++;
-
-		vector<TranslationRangeItem> ts;
-
-//TODO: views::set_union() and views::gzip_transform() and we wouldn't need to create so many variables.
-
-		ranges::set_union(
-			prev,
-			views::iota(0,n) | views::transform([&](int i){return TranslationRangeItem{.id=id,.ri=i, .tr={.x=0,.y=0}}; }),
-			back_inserter(ts),
-			{},
-			&TranslationRangeItem::ri,
-			&TranslationRangeItem::ri);
-
-		auto rects = views::iota(0,n) | views::transform([&](int i){return rectangles_[i]+ts[i].tr;});
-		ranges::copy(rects, begin(rectangles));
-
-		bitset<30> partition;
-
-		auto rec_select_partition=[&](int ri, auto&& rec_select_partition)->void{
-
-			auto rg = ranges::equal_range(rect_links, ri, {}, &RectLink::i)
-						| views::filter([&](const RectLink& rl){return rectangles[ri][maxCompactRectDim] == rectangles[rl.j][minCompactRectDim];});
-
-			ranges::for_each(rg, [&](const RectLink& rl){
-					partition[rl.j]=1;
-					printf("partition[%d]=1\n", rl.j);
-					rec_select_partition(rl.j, rec_select_partition);
-			});
-		};
-
-		auto rg = rectangles | views::transform([&](const MyRect& r){return r[minCompactRectDim];});
-		const int frame_min = ranges::min(rg);
-		const int next_min = ranges::min(rg | views::filter([&](int value){return value != frame_min;}));
-
-		printf("frame_min=%d\n", frame_min);
-		printf("next_min=%d\n", next_min);
-
-		auto rng = views::iota(0,n) | views::filter([&](int i){return rectangles[i][minCompactRectDim]==frame_min;});
-
-		for (int ri : rng)
-		{
-			partition[ri] = 1;
-			printf("partition[%d] = 1\n", ri);
-			rec_select_partition(ri, rec_select_partition);
-		}
-
-		auto r = rect_links | views::filter([&](const RectLink& e){return partition[e.i] > partition[e.j];})
-				| views::transform([&](const RectLink& e){return rectangles[e.j][minCompactRectDim]-rectangles[e.i][maxCompactRectDim];}) ;
-
-		vector<TranslationRangeItem> translation_ranges;
-
-		if (ranges::empty(r))
-			return translation_ranges;
-
-		MyPoint tr={.x=0, .y=0};
-		tr[update_direction] = min<int>(ranges::min(r), next_min - frame_min);
-
-		auto rg2 = views::iota(0,n) | views::filter([&](int i){return partition[i]==1;})
-					| views::transform([&](int i){return TranslationRangeItem{.id=id,.ri=i,.tr=tr};});
-
-		ranges::for_each(rg2, [&](const TranslationRangeItem& item){const auto [id, ri, tr]=item; rectangles[ri]+=tr;});
-
-		auto rg3 = views::iota(0,n) | views::filter([&](int i){return rectangles[i][minCompactRectDim] != rectangles_[i][minCompactRectDim];})
-						| views::transform([&](int i){MyPoint tr;
-										tr[update_direction] = rectangles[i][minCompactRectDim] - rectangles_[i][minCompactRectDim];
-										return TranslationRangeItem{.id=id, .ri=i, .tr=tr};
-									});
-		for (const auto [id, ri, tr] : rg3)
-		{
-			printf("{.id=%d, .ri=%d, .tr={.x=%d, .y=%d}},\n", id, ri, tr.x, tr.y);
-		}
-
-		ranges::copy(rg3, back_inserter(translation_ranges));
-		return translation_ranges;
-	};
-
-//10: just had to choose a number. Should not be needed with C++23 partial_fold()
-// Cf https://stackoverflow.com/questions/74042325/listing-all-intermediate-recurrence-results
-
-	partial_sum(vv.begin(), vv.end(), vv.begin(),
-				[&](const vector<TranslationRangeItem>& prev, const vector<TranslationRangeItem>&){
-					return next(prev);}
-				);
-
-	auto rg = vv | views::join;
-
-        printf("rg = vv | views::join\n");
-
-        for (const auto [id, ri, tr] : rg)
-        {
-                printf("{.id=%d, .ri=%d, .tr={.x=%d, .y=%d}},\n", id, ri, tr.x, tr.y);
-        }
-
-//TODO: use views::left_fold() when it hopefully becomes available in C++23. It might clarify the design.
-// Cf https://stackoverflow.com/questions/74042325/listing-all-intermediate-recurrence-results
-
-//TODO: use C++23 chunk_by()
-
-	const int nb = 1 + ranges::max(rg | views::transform(&TranslationRangeItem::id));
-
-	id = ranges::min( views::iota(0,nb), {}, [&](int id){
-
-			vector<MyRect> rectangles = rectangles_;
-			ranges::for_each(ranges::equal_range(rg, id, {}, &TranslationRangeItem::id),
-							[&](const TranslationRangeItem& item){const auto [id, ri, tr]=item; rectangles[ri]+=tr;});
-
-			auto rg1 = logical_edges |
-				views::transform([&](const auto& le){ return rectangle_distance(rectangles[le.from],rectangles[le.to]);	});
-
-			auto rg2 = ranges::equal_range(rg, id, {}, &TranslationRangeItem::id) |
-				views::transform([&](const TranslationRangeItem& item){const auto [id,i,tr]=item; return abs(tr.x) + abs(tr.y);});
-
-			const int sigma_edge_distance = accumulate(ranges::begin(rg1), ranges::end(rg1),0);
-			const int sigma_translation = accumulate(ranges::begin(rg2), ranges::end(rg2),0);
-			const auto [width, height] = dimensions(compute_frame(rectangles));
-
-			printf("sigma_edge_distance = %d\n", sigma_edge_distance);
-			printf("sigma_translation = %d\n", sigma_translation);
-			printf("[.width=%d, .height=%d]\n", width, height);
-
-			int cost = width + height + sigma_edge_distance + sigma_translation ;
-                        return cost;}
-                );
-	printf("id=%d\n", id);
-
-	rectangles = rectangles_;
-	ranges::for_each(ranges::equal_range(rg, id, {}, &TranslationRangeItem::id),
-			[&](const TranslationRangeItem& item){const auto [id, ri, tr]=item; rectangles[ri]+=tr;});
-}
-
-
-int main(int argc, char* argv[])
-{
-	vector<MyRect> rectangles = input_rectangles;
-	compact(EAST_WEST, rect_links, logical_edges, rectangles);
-
-	return 0;
+"frame":
+{"left":0,"right":1126,"top":0,"bottom":608},
+"translatedBoxes":[
+{"id":0,"translation":{"x":977,"y":426}},//0
+{"id":1,"translation":{"x":977,"y":250}},//1
+{"id":2,"translation":{"x":818,"y":266}},//2
+{"id":3,"translation":{"x":942,"y":10}},//3
+{"id":4,"translation":{"x":176,"y":74}},//4
+{"id":6,"translation":{"x":624,"y":26}},//5
+{"id":12,"translation":{"x":479,"y":378}},//6
+{"id":13,"translation":{"x":388,"y":10}},//7
+{"id":14,"translation":{"x":176,"y":314}},//8
+{"id":15,"translation":{"x":388,"y":138}},//9
+{"id":19,"translation":{"x":10,"y":266}},//10
+{"id":20,"translation":{"x":10,"y":154}},//11
+{"id":21,"translation":{"x":10,"y":426}},//12
+{"id":26,"translation":{"x":673,"y":266}},//13
+{"id":30,"translation":{"x":369,"y":378}}//14
+],
+"rectangles":[
+{.m_left=0+977, .m_right=119+977, .m_top=0+426, .m_bottom=152+426},//0
+{.m_left=0+977, .m_right=119+977, .m_top=0+250, .m_bottom=136+250},//1
+{.m_left=0+818, .m_right=119+818, .m_top=0+266, .m_bottom=168+266},//2
+{.m_left=0+942, .m_right=154+942, .m_top=0+10, .m_bottom=200+10},//3
+{.m_left=0+176, .m_right=154+176, .m_top=0+74, .m_bottom=200+74},//4
+{.m_left=0+624, .m_right=126+624, .m_top=0+26, .m_bottom=200+26},//6
+{.m_left=0+479, .m_right=154+479, .m_top=0+378, .m_bottom=200+378},//12
+{.m_left=0+388, .m_right=154+388, .m_top=0+10, .m_bottom=88+10},//13
+{.m_left=0+176, .m_right=154+176, .m_top=0+314, .m_bottom=200+314},//14
+{.m_left=0+388, .m_right=196+388, .m_top=0+138, .m_bottom=200+138},//15
+{.m_left=0+10, .m_right=126+10, .m_top=0+266, .m_bottom=120+266},//19
+{.m_left=0+10, .m_right=126+10, .m_top=0+154, .m_bottom=72+154},//20
+{.m_left=0+10, .m_right=126+10, .m_top=0+426, .m_bottom=72+426},//21
+{.m_left=0+673, .m_right=105+673, .m_top=0+266, .m_bottom=200+266},//26
+{.m_left=0+369, .m_right=70+369, .m_top=0+378, .m_bottom=88+378}//30
+],
+"links":[
+{.from=0, .to=2},
+{.from=1, .to=2},
+{.from=2, .to=0},
+{.from=2, .to=1},
+{.from=2, .to=3},
+{.from=2, .to=9},
+{.from=3, .to=2},
+{.from=4, .to=5},
+{.from=4, .to=6},
+{.from=4, .to=9},
+{.from=4, .to=11},
+{.from=5, .to=4},
+{.from=6, .to=4},
+{.from=6, .to=8},
+{.from=7, .to=9},
+{.from=8, .to=6},
+{.from=8, .to=9},
+{.from=8, .to=12},
+{.from=9, .to=2},
+{.from=9, .to=4},
+{.from=9, .to=7},
+{.from=9, .to=8},
+{.from=9, .to=10},
+{.from=9, .to=13},
+{.from=9, .to=14},
+{.from=10, .to=9},
+{.from=10, .to=11},
+{.from=10, .to=12},
+{.from=11, .to=4},
+{.from=11, .to=10},
+{.from=12, .to=8},
+{.from=12, .to=10},
+{.from=13, .to=9},
+{.from=14, .to=9}
+]
 }
