8c53a1dc5cdbb1e664696e1e7ea241db138c4add
Ludovic Aubert <ludo.aubert@gmail.com>
2023-01-17 18:07:12 +0100

    Add files via upload

--- table_edit.js
+++ table_edit.js
@@ -678,19 +678,6 @@ function dropColor()
 	drawDiag();
 }
 
-const getBase64FromUrl = async (url) => {
-	const data = await fetch(url);
-	const blob = await data.blob();
-	return new Promise((resolve) => {
-		const reader = new FileReader();
-		reader.readAsDataURL(blob);
-		reader.onloadend = () => {
-			const base64data = reader.result;
-      			resolve(base64data);
-		}
-	});
-}
-
 
 function dropPicture()
 {
