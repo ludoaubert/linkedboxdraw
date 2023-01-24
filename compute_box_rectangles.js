fd668396b1246a139d5a3981152a2d31406ce585
Ludovic Aubert <ludo.aubert@gmail.com>
2023-01-24 17:27:29 +0100

    Add files via upload

--- compute_box_rectangles.js
+++ compute_box_rectangles.js
@@ -21,46 +21,64 @@ const MONOSPACE_FONT_PIXEL_WIDTH=7;
 const CHAR_RECT_HEIGHT=16;	// in reality 14,8 + 1 + 1 (top and bottom padding) = 16,8
 const RECTANGLE_BOTTOM_CAP=200;
 
+function compute_column_width(field, key_distrib)
+{
+	const column_name = field.name;
+
+	var column_width=0;
+
+	if (key_distrib["PKFK"])
+	{
+//at least one 'PK FK' is present
+		column_width = ("PK FK " + column_name).length ;
+	}
+	else if (key_distrib["PK"] || key_distrib["FK"])
+	{
+//no 'PK FK' is present, but at least one PK|FK is present.
+		column_width = ("PK " + column_name).length ;
+	}
+	else
+	{
+//no 'PK FK' is present. no PK|FK either.
+		column_width = column_name.length ;
+	}	
+	
+	return column_width;
+}
+
 function compute_box_rectangle(box)
 {
-        const {title,id,fields} = box;
+	const {title,id,fields} = box;
 
 	const key_distrib = compute_key_distrib(fields) ;
 
-	var nr_col = 0 ;
-	var width = 2*4 + title.length * MONOSPACE_FONT_PIXEL_WIDTH ;
-	var max_width = width;
+	const widths = [
+		2*4 + title.length * MONOSPACE_FONT_PIXEL_WIDTH,
+		fields.filter(field => field?.type == undefined)
+				.map(field => compute_column_width(field, key_distrib) * MONOSPACE_FONT_PIXEL_WIDTH),
+		fields.filter(field => field?.type == "image")
+				.map(field => field.width)
+	].flat();
+	
+	max_width = Math.max(...widths);
+	
+	const heights = [
+		8 + CHAR_RECT_HEIGHT,
+		fields.filter(field => field?.type == undefined)
+				.map(field => CHAR_RECT_HEIGHT),
+		fields.filter(field => field?.type == "image")
+				.map(field => field.height)
+	].flat()
 
-	for (const field of fields)
-	{
-		nr_col++ ;
-		const column_name = field.name;
+	const height = heights.reduce((a,b) => a + b, 0);
 
-		var column_width=0;
+	const rec = {
+		left:0,
+		right:max_width,
+		top:0,
+		bottom:Math.min(height, RECTANGLE_BOTTOM_CAP)
+	} ;
 
-		if (key_distrib["PKFK"])
-		{
-	//at least one 'PK FK' is present
-			column_width = ("PK FK " + column_name).length ;
-		}
-		else if (key_distrib["PK"] || key_distrib["FK"])
-		{
-	//no 'PK FK' is present, but at least one PK|FK is present.
-			column_width = ("PK " + column_name).length ;
-		}
-		else
-		{
-	//no 'PK FK' is present. no PK|FK either.
-			column_width = column_name.length ;
-		}
-
-		max_width = Math.max(column_width * MONOSPACE_FONT_PIXEL_WIDTH, max_width);
-	}
-
-	const bottom = 8 + CHAR_RECT_HEIGHT * (nr_col+1) ;
-
-	const rec = {left:0, right:max_width, top:0, bottom:Math.min(bottom, RECTANGLE_BOTTOM_CAP)} ;
-
-        return rec;
+	return rec;
 }
 
