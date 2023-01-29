4dea3d66d6d9a49c0064de202afb7ad8a97f9f34
edoudabear <edou.aubert@gmail.com>
2023-01-29 21:55:41 +0100

    titi

--- diagload.js
+++ diagload.js
@@ -1,6 +1,6 @@
 import sample_contexts from "./contexts.json" assert {type: "json"};
 import sample_diagdata from "./diagdata.json" assert {type: "json"};
-import {Module} from "/Module.js"
+import {Module} from "./Module.js"
 
 import {init} from "./table_edit.js"
 
