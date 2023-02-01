74c9108e1d6468a7f9047ca89474eab44f3e9816
Ludovic Aubert <ludo.aubert@gmail.com>
2023-02-01 15:30:21 +0100

    Add files via upload

--- iocomponent.js
+++ iocomponent.js
@@ -99,3 +99,16 @@ function download(filename, jsonData) {
   document.body.removeChild(element);
 }
 
+function getFileData(element)
+{
+	if (element.files && element.files[0])
+	{
+		return new Promise((resolve) => {
+			var reader = new FileReader();
+			reader.addEventListener('load', (e) => {
+				resolve(e.target.result);
+			})
+			reader.readAsBinaryString(element.files[0]);
+		}
+	}	
+}
