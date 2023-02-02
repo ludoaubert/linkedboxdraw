260484a26c49a8303348b1a363c04236fb77cebc
Ludovic Aubert <ludo.aubert@gmail.com>
2023-02-02 13:07:45 +0100

    Add files via upload

--- diagload.js
+++ diagload.js
@@ -2,11 +2,9 @@ import sample_contexts from "./contexts.json" assert {type: "json"};
 
 import {default as createMyModule} from "./latuile-origine.js";
 import {init, mydata, data, resetData, displayCurrent} from "./table_edit.js";
-import {getFileData} from "./iocomponent.js";
+import {getFileData, download} from "./iocomponent.js";
 
 export {mycontexts, contexts, resetContexts, drawDiag};
-
-export default {Module};
 
 var Module;
 
@@ -27,6 +25,91 @@ var sizer = 0;
 // FRAME_MARGIN is duplicated in table_input.js, diagload.js and topo_space.js
 const FRAME_MARGIN = 20;
 const RECT_BORDER = 20;
+
+
+//TODO: refactor with compute_links(selectedContextIndex)
+
+function download2(filename) {
+	var element = document.createElement('a');
+
+	const {boxes, links} = mydata;
+
+	const rectangles = boxes.map(box => compute_box_rectangle(box));
+
+	const hex = (i,n) => i.toString(16).padStart(n,'0');
+
+	const rectdim = rectangles.map(r => hex(r.right-r.left,3)+hex(r.bottom-r.top,3));
+	console.log(rectdim);
+
+	const slinks = links.filter(lk => lk.from != lk.to)
+						.filter(lk => lk.category != "TR2")
+						.map(lk => [lk.from, lk.to])
+						.map(lk => JSON.stringify(lk))
+						.filter(function(lk, pos, self){
+									return self.indexOf(lk) == pos;}
+						) //removing duplicates
+						.map(lk => JSON.parse(lk))
+						.flat()
+						.map(i => hex(i,3))
+						.join('');
+	console.log(slinks);
+
+	latuile = Module.cwrap("latuile","string",["string","string"]);
+	bombix = Module.cwrap("bombix","string",["string","string","string","string"]);
+
+	const jsonResponse = latuile(rectdim.join(''), slinks);
+	console.log(jsonResponse);
+
+	mycontexts = JSON.parse(jsonResponse);
+	mycontexts.rectangles = rectangles;
+	mycontexts.contexts = mycontexts.contexts.map(
+		({frame, translatedBoxes}) => {
+			const {left,right,top,bottom} = frame;
+			const sframe = [left, right, top, bottom]
+							.map(i => hex(i,4))
+							.join('');
+			console.log(sframe);
+
+			const translations = translatedBoxes
+							.map(({id,translation})=>[translation.x,translation.y])
+							.flat()
+							.map(i => hex(i,3))
+							.join('');
+			console.log(translations);
+
+			const ids = translatedBoxes.map(tB => tB.id);
+			const rectdim_ = translatedBoxes
+							.map(({id}) => rectdim[id])
+							.join('');
+			console.log(rectdim_);
+			console.assert(rectdim_.size == translations.size);
+			console.log(links);
+			const links_ = links.filter(lk => lk.from != lk.to)
+						.filter(lk => lk.category != "TR2")
+						.map(lk => ({from:lk.from, to:lk.to}))
+						.filter(lk => ids.indexOf(lk.from) != -1 && ids.indexOf(lk.to) != -1)
+						.map(lk => [ids.indexOf(lk.from), ids.indexOf(lk.to)])
+						.map(lk => JSON.stringify(lk))
+						.filter(function(lk, pos, self){
+								return self.indexOf(lk) == pos;}
+							) //removing duplicates
+						.map(lk => JSON.parse(lk))
+						.flat()
+						.map(i => hex(i,2))
+						.join('');
+			console.log(links_);
+			const json2 = bombix(rectdim_, translations, sframe, links_);
+			console.log(json2);
+			const polylines = JSON.parse(json2);
+			const polylines2 = polylines.map(({polyline,from,to})=>({polyline, from:ids[from], to:ids[to]}));
+			console.log(polylines2);
+
+			return {frame, translatedBoxes, links:polylines2};
+		}
+	);
+
+	download(filename, mycontexts);
+}
 
 
 function selectElement(elmnt)
@@ -580,6 +663,9 @@ window.main = function main()
 	});
 	let gfo = document.querySelector("input[id=gfo]");
 	gfo.addEventListener("click", (event) => download(gfo.previousElementSibling.value, mycontexts));
+	
+	let csl = document.querySelector("input[id=csl]");
+	csl.addEventListener("click", (event) => {download2(csl.previousElementSibling.value); drawDiag();});
 
 	createMyModule().then(function(mymod){
 		Module = mymod;
--- table_edit.js
+++ table_edit.js
@@ -1,7 +1,7 @@
 import sample_diagdata from "./diagdata.json" assert {type: "json"};
 
 import {mycontexts, contexts, resetContexts, drawDiag} from "./diagload.js";
-import {default as Module}  from "./diagload.js";
+
 import {download} from "./iocomponent.js";
 import {getFileData} from "./iocomponent.js";
 
@@ -103,90 +103,6 @@ function newDiagram() {
 }
 
 
-//TODO: refactor with compute_links(selectedContextIndex)
-
-function download2(filename) {
-	var element = document.createElement('a');
-
-	const {boxes, links} = mydata;
-
-	const rectangles = boxes.map(box => compute_box_rectangle(box));
-
-	const hex = (i,n) => i.toString(16).padStart(n,'0');
-
-	const rectdim = rectangles.map(r => hex(r.right-r.left,3)+hex(r.bottom-r.top,3));
-	console.log(rectdim);
-
-	const slinks = links.filter(lk => lk.from != lk.to)
-						.filter(lk => lk.category != "TR2")
-						.map(lk => [lk.from, lk.to])
-						.map(lk => JSON.stringify(lk))
-						.filter(function(lk, pos, self){
-									return self.indexOf(lk) == pos;}
-						) //removing duplicates
-						.map(lk => JSON.parse(lk))
-						.flat()
-						.map(i => hex(i,3))
-						.join('');
-	console.log(slinks);
-
-	latuile = Module.cwrap("latuile","string",["string","string"]);
-	bombix = Module.cwrap("bombix","string",["string","string","string","string"]);
-
-	const jsonResponse = latuile(rectdim.join(''), slinks);
-	console.log(jsonResponse);
-
-	mycontexts = JSON.parse(jsonResponse);
-	mycontexts.rectangles = rectangles;
-	mycontexts.contexts = mycontexts.contexts.map(
-		({frame, translatedBoxes}) => {
-			const {left,right,top,bottom} = frame;
-			const sframe = [left, right, top, bottom]
-							.map(i => hex(i,4))
-							.join('');
-			console.log(sframe);
-
-			const translations = translatedBoxes
-							.map(({id,translation})=>[translation.x,translation.y])
-							.flat()
-							.map(i => hex(i,3))
-							.join('');
-			console.log(translations);
-
-			const ids = translatedBoxes.map(tB => tB.id);
-			const rectdim_ = translatedBoxes
-							.map(({id}) => rectdim[id])
-							.join('');
-			console.log(rectdim_);
-			console.assert(rectdim_.size == translations.size);
-			console.log(links);
-			const links_ = links.filter(lk => lk.from != lk.to)
-						.filter(lk => lk.category != "TR2")
-						.map(lk => ({from:lk.from, to:lk.to}))
-						.filter(lk => ids.indexOf(lk.from) != -1 && ids.indexOf(lk.to) != -1)
-						.map(lk => [ids.indexOf(lk.from), ids.indexOf(lk.to)])
-						.map(lk => JSON.stringify(lk))
-						.filter(function(lk, pos, self){
-								return self.indexOf(lk) == pos;}
-							) //removing duplicates
-						.map(lk => JSON.parse(lk))
-						.flat()
-						.map(i => hex(i,2))
-						.join('');
-			console.log(links_);
-			const json2 = bombix(rectdim_, translations, sframe, links_);
-			console.log(json2);
-			const polylines = JSON.parse(json2);
-			const polylines2 = polylines.map(({polyline,from,to})=>({polyline, from:ids[from], to:ids[to]}));
-			console.log(polylines2);
-
-			return {frame, translatedBoxes, links:polylines2};
-		}
-	);
-
-	download(filename, mycontexts);
-}
-
 function loadPicture(blob)
 {
 	currentPictureIndex = mydata.pictures.length;
@@ -287,8 +203,6 @@ function init() {
 	});
 	let dfo = document.querySelector("input[id=dfo]");
 	dfo.addEventListener("click", (event) => download(dfo.previousElementSibling.value, mydata));
-	let csl = document.querySelector("input[id=csl]");
-	csl.addEventListener("click", (event) => {download2(csl.previousElementSibling.value); drawDiag();});
 
 	picturesCombo.addEventListener("change", (event) => {currentPictureIndex = -1; displayCurrent();});
 	let add_pic = document.querySelector("input[id=add_pic]");
