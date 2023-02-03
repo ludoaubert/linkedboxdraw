1816bb2a31a4ab326fac55837ce244faeddf60c2
Ludovic Aubert <ludo.aubert@gmail.com>
2023-02-03 14:00:27 +0100

    Add files via upload

--- table_edit.js
+++ table_edit.js
@@ -262,6 +262,12 @@ function createMutationObserver()
 		mutations.forEach(function (mutation) {
 			const id = mutation.target.parentElement.id;
 			const data = mutation.target.data;
+			const regexpId = /b([0-9]+)f([0-9]+)/;
+			const match = id.match(regexpId);
+			console.log(`box: ${match[1]} / field: ${match[2]}.`);
+			const boxIndex = parseInt(match[1]);
+			const fieldIndex = parseInt(match[2]);
+			mydata.boxes[boxIndex].fields[fieldIndex].name = data;
 		});
 	};
 
