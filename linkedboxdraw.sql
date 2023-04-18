7e6e32de9c0387ef2449b554218d0128395c7f94
Ludovic Aubert <ludo.aubert@gmail.com>
2023-04-18 11:41:16 +0200

    Add files via upload

--- linkedboxdraw.sql
+++ linkedboxdraw.sql
@@ -229,3 +229,24 @@ SELECT 1 AS diagramId, height, width, name, base64
 FROM cte_pictures_pivot;
 
 --reproducing JSON from tables:
+WITH cte_fields AS (
+    SELECT boxPosition, 
+                        '[' || group_concat(json_object('name',name,
+                                                'isPrimaryKey',json(CASE isPrimaryKey WHEN 0 THEN 'false' WHEN 1 THEN 'true' END),
+                                                'isForeignKey',json(CASE isForeignKey WHEN 0 THEN 'false' WHEN 1 THEN 'true' END))
+                                    ) || ']' AS fields
+    FROM field
+    WHERE diagramId=1
+    GROUP BY boxPosition
+) , cte_boxes AS (
+    SELECT group_concat( json_object('title', title, 'id', position, 'fields', fields), ',') AS boxes  
+    FROM box
+    JOIN cte_fields ON box.position=cte_fields.boxPosition
+    WHERE box.diagramId=1
+) , cte_doc AS (
+    SELECT json_object('documentTitle', diagram.title, 'boxes', cte_boxes.boxes)
+    FROM diagram 
+    CROSS JOIN cte_boxes
+    WHERE diagram.id=1
+)
+SELECT * FROM cte_doc;
