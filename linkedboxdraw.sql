adf46590f5c3e59ae6463659570ab8bfe4b8904f
Ludovic Aubert <ludo.aubert@gmail.com>
2023-04-18 11:55:44 +0200

    Add files via upload

--- linkedboxdraw.sql
+++ linkedboxdraw.sql
@@ -247,11 +247,16 @@ WITH cte_fields AS (
     SELECT '[' || group_concat( json_object('from',fromBoxPosition,'fromField',fromFieldPosition,'fromCardinality',fromCardinality,'to',toBoxPosition,'toField',toFieldPosition,'toCardinality',toCardinality,'category',category), ',') || ']' AS links
     FROM link
     WHERE diagramId=1
-) , cte_doc AS (
-    SELECT json_object('documentTitle', diagram.title, 'boxes', cte_boxes.boxes, 'links', cte_links.links)
+) ,cte_pictures AS (
+    SELECT '[' || group_concat( json_object('height', height, 'width', width, 'name', name, 'base64', base64)) || ']' AS pictures
+    FROM picture
+    WHERE diagramId=1
+), cte_doc AS (
+    SELECT json_object('documentTitle', diagram.title, 'boxes', cte_boxes.boxes, 'links', cte_links.links, 'pictures', cte_pictures.pictures)
     FROM diagram 
     CROSS JOIN cte_boxes
     CROSS JOIN cte_links
+    CROSS JOIN cte_pictures
     WHERE diagram.id=1
 )
 SELECT * FROM cte_doc;
