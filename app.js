65a13433caa91e49d0a2c5cffcb6b918a7cc25c8
Ludovic Aubert <ludo.aubert@gmail.com>
2023-05-03 13:55:10 +0200

    url=/list_diagrams

--- app.js
+++ app.js
@@ -7,14 +7,26 @@ const port = 3000;
 const SQLITE_TOOLS_DIR='C:/lulu/sqlite-tools-win32-x86-3410200'
 
 const server = http.createServer((req, res) => {
-	
-  exec(`${SQLITE_TOOLS_DIR}/sqlite3 linkedboxdraw.db ".read select_document.sql"`, (error, stdout, stderr) => {
-    console.log("STDOUT:", stdout, ", STDERR:", stderr);
-	  res.statusCode = 200;
-      res.setHeader('Content-Type', 'application/json');
+
+    if (req.url=='/list_diagrams')
+    {
+        exec(`${SQLITE_TOOLS_DIR}/sqlite3 linkedboxdraw.db "SELECT json_group_array(json_object('title',title, 'deleted', deleted, 'guid', guid)) FROM diagram"`,(error, stdout, stderr) => {
+          console.log("STDOUT:", stdout, ", STDERR:", stderr);
+	      res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          res.end(stdout);
+		});
+    }
+	else
+    {
+        exec(`${SQLITE_TOOLS_DIR}/sqlite3 linkedboxdraw.db ".read select_document.sql"`, (error, stdout, stderr) => {
+          console.log("STDOUT:", stdout, ", STDERR:", stderr);
+	      res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
   //res.end('Hello World');
-      res.end(stdout);
-});
+          res.end(stdout);
+        });
+    }
 
 });
 
