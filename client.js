35852dfc5b9d26bebce6c9c1dae4f973643ffd13
Ludovic Aubert <ludo.aubert@gmail.com>
2023-05-03 17:17:19 +0200

    Add files via upload

--- client.js
+++ client.js
@@ -5,7 +5,7 @@ var currentDigramIndex = -1;
 var diagrams=null;
 
 async function initClient() {
-    const response = await fetch("http://127.0.0.1/list_diagrams");
+    const response = await fetch("http://127.0.0.1:3000/list_diagrams");
     const jsonData = await response.json();
 	diagrams = JSON.parse(jsonData);
 }
