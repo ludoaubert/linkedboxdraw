888f36abe1eaf64768ed55743cc7b7738244fe0b
Ludovic Aubert <ludo.aubert@gmail.com>
2023-05-12 13:58:27 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -8,8 +8,11 @@ const app = express();
 app.use(cors());
 const port = 3000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+//app.use(express.json());
+//app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb',extended: true}));
 
 const SQLITE_TOOLS_DIR='C:/lulu/sqlite-tools-win32-x86-3410200';
 const DEPLOY_DIR='C:/tmp/linkedboxdraw-master';
