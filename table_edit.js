90e8f4e6d6a16b82e2e33906155f22c90178d5be
Ludovic Aubert <ludo.aubert@gmail.com>
2023-05-13 23:11:08 +0200

    Add files via upload

--- table_edit.js
+++ table_edit.js
@@ -4,7 +4,7 @@ import {mycontexts, contexts, resetContexts, setContexts, drawDiag, compute_link
 
 import {download} from "./iocomponent.js";
 import {getFileData} from "./iocomponent.js";
-import {compute_box_rectangle} from "./compute_box_rectangle.js"
+import {compute_box_rectangle} from "./compute_box_rectangles.js"
 
 export {init, mydata, data, resetData, setData, displayCurrent, createMutationObserver};
 
