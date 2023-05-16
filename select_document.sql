c3d9ef6bd6643a56f65ac81df5577177d0cac863
Ludovic Aubert <ludo.aubert@gmail.com>
2023-05-16 10:47:10 +0200

    select_document.sql box comments and field values

--- select_document.sql
+++ select_document.sql
@@ -30,6 +30,18 @@ WITH cte_diagram AS (
     FROM link
 	CROSS JOIN cte_diagram
     WHERE diagramId=cte_diagram.id
+), cte_field_values AS (
+    SELECT json_group_array( json_object('box', box.title, 'field', field.name, 'value', fieldValue.fieldValue)) AS field_values
+    FROM fieldValue
+    JOIN box ON box.diagramId = fieldValue.diagramId AND box.position=fieldValue.boxPosition
+    JOIN field ON field.diagramId = fieldValue.diagramId AND field.position=fieldValue.fieldPosition AND box.position = fieldValue.boxPosition
+    CROSS JOIN cte_diagram
+    WHERE fieldValue.diagramId=cte_diagram.id
+), cte_box_comments AS (
+    SELECT json_group_array( json_object('box', box.title, 'comment', boxComment.bComment)) AS box_comments
+    FROM boxComment
+    JOIN box ON box.diagramId=boxComment.diagramId AND box.position=boxComment.boxPosition
+    WHERE boxComment.diagramId = cte_diagram.id
 ) ,cte_pictures AS (
     SELECT json_group_array( json_object('height', height, 'width', width, 'name', name, 'base64', base64)) AS pictures
     FROM picture
@@ -38,8 +50,8 @@ WITH cte_diagram AS (
 ), cte_doc AS (
     SELECT json_object('documentTitle', diagram.title, 
 		       'boxes', json(cte_boxes.boxes), 
-		       'values', json_array(),
-		       'boxComments', json_array(),
+		       'values', json(cte_field_values.field_values),
+		       'boxComments', json(cte_box_comments.box_comments),
 		       'fieldComments', json_array(),
 		       'links', json(cte_links.links),
 		       'fieldColors', json_array(),
@@ -47,6 +59,8 @@ WITH cte_diagram AS (
     FROM diagram 
     CROSS JOIN cte_boxes
     CROSS JOIN cte_links
+    CROSS JOIN cte_field_values
+    CROSS JOIN cte_box_comments
     CROSS JOIN cte_pictures
 	CROSS JOIN cte_diagram
     WHERE diagram.id=cte_diagram.id
