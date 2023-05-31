9b2346b506c21dfc18d4bfc6fc03a8231d0efdb3
Ludovic Aubert <ludo.aubert@gmail.com>
2023-05-31 22:30:53 +0200

    Update table_edit.js

--- table_edit.js
+++ table_edit.js
@@ -200,6 +200,115 @@ function init() {
 			.forEach(button => {
 				button.addEventListener("click", (event) => switchCollapsible(button));
 			});
+	
+	let fi = document.querySelector("input[id=fi]");
+
+	fi.addEventListener("change", (event) => {
+		getFileData(fi).then(function(result){
+			const json = JSON.parse(result);
+			if ("data" in json && "contexts" in json)
+			{
+				setData(json.data);
+				setContexts(json.contexts);
+				drawDiag();
+				currentBoxIndex = -1;
+				displayCurrent();
+			}
+			else if ("documentTitle" in json && "boxes" in json && "links" in json)
+			{
+				setData(json);
+				
+				const {boxes, links} = json;
+	
+				const rectangles = compute_box_rectangles(boxes);
+	
+				const hex = (i,n) => i.toString(16).padStart(n,'0');
+	
+				const rectdim = rectangles
+						.map(r => hex(r.right-r.left,3)+hex(r.bottom-r.top,3));
+				console.log(rectdim);
+						
+				const slinks = links.filter(lk => lk.from != lk.to)
+						.filter(lk => lk.category != "TR2")
+						.map(lk => [lk.from, lk.to])
+						.map(lk => JSON.stringify(lk))
+						.filter(function(lk, pos, self){
+									return self.indexOf(lk) == pos;}
+						) //removing duplicates
+						.map(lk => JSON.parse(lk))
+						.flat()
+						.map(i => hex(i,3))
+						.join('');
+				console.log(slinks);
+	
+				latuile = Module.cwrap("latuile","string",["string","string"]);
+				bombix = Module.cwrap("bombix","string",["string","string","string","string"]);	
+
+
+				const jsonResponse = latuile(rectdim.join(''), slinks);
+				console.log(jsonResponse);
+	
+				data = JSON.parse(jsonResponse);
+	
+				data.rectangles = rectangles;
+	
+				data.contexts = data.contexts.map(
+					({frame, translatedBoxes}) => {
+						const {left,right,top,bottom} = frame;
+						const sframe = [left, right, top, bottom]
+								.map(i => hex(i,4))
+								.join('');
+						console.log(sframe);
+			
+						const translations = translatedBoxes
+							.map(({id,translation})=>[translation.x,translation.y])
+							.flat()
+							.map(i => hex(i,3))
+							.join('');
+						console.log(translations);
+			
+						const ids = translatedBoxes.map(tB => tB.id);
+			
+						const rectdim_ = translatedBoxes
+							.map(({id}) => rectdim[id])
+							.join('');
+						console.log(rectdim_);
+						console.assert(rectdim_.size == translations.size);
+						console.log(links);
+						const links_ = links
+							.filter(lk => lk.from != lk.to)
+							.filter(lk => lk.category != "TR2")
+							.map(lk => ({from:lk.from, to:lk.to}))
+							.filter(lk => ids.indexOf(lk.from) != -1 && ids.indexOf(lk.to) != -1)
+							.map(lk => [ids.indexOf(lk.from), ids.indexOf(lk.to)])
+							.map(lk => JSON.stringify(lk))
+							.filter(function(lk, pos, self){
+										return self.indexOf(lk) == pos;}
+							) //removing duplicates
+							.map(lk => JSON.parse(lk))							
+							.flat()
+							.map(i => hex(i,2))
+							.join('');	
+						console.log(links_);
+						const json2 = bombix(rectdim_, translations, sframe, links_);
+					console.log(json2);
+					const polylines = JSON.parse(json2);
+					const polylines2 = polylines.map(({polyline,from,to})=>({polyline, from:ids[from], to:ids[to]}));
+					console.log(polylines2);
+			
+					return {frame, translatedBoxes, links:polylines2};
+				});
+	
+				setContexts(data);
+				drawDiag();
+				currentBoxIndex = -1;
+				displayCurrent();
+			}
+	});
+
+	let fo = document.querySelector("input[id=fo]");
+	fo.addEventListener("click", () => download(fo.previousElementSibling.value, {data:mydata, contexts:mycontexts}));
+	
 	let dfi = document.querySelector("input[id=dfi]");
 
 	dfi.addEventListener("change", (event) => {
