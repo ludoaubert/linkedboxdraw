2a53a710b19b2317568305d67bceb96d411125b4
Ludovic Aubert <ludo.aubert@gmail.com>
2023-07-16 15:45:00 +0200

    Add files via upload

--- compute_decision_tree.cpp
+++ compute_decision_tree.cpp
@@ -9,10 +9,6 @@ using namespace std;
 struct Edge {
 	int from;
 	int to;
-
-    // note: to is ignored by these comparison operators
-    friend bool operator== (const Edge e1, const Edge e2) { return e1.from == e2.from; }
-    friend auto operator<=>(const Edge e1, const Edge e2) { return e1.from <=> e2.from; }
 };
 
 
@@ -263,7 +259,7 @@ vector<DecisionTreeNode> compute_decision_tree(int nr_input_rectangles, int nr_e
 	vector<DecisionTreeNode> decision_tree;
 	
 	auto build_decision_tree = [&](int parent_index, auto&& build_decision_tree)->void{
-		printf("enter build_decision_tree()\n");
+		//printf("enter build_decision_tree()\n");
 		vector<int> chemin;
 		for (int index=parent_index; index != -1; index = decision_tree[index].parent_index)
 		{
@@ -294,27 +290,31 @@ vector<DecisionTreeNode> compute_decision_tree(int nr_input_rectangles, int nr_e
 		for (int h : views::iota(nr_input_rectangles, nr_emplacements))
 		{
 			int eh = emplacement(h);
-			printf("parent_index=%d\n", parent_index);
-			printf("emplacement(h=%d)=%d\n", h, eh);
+			//printf("parent_index=%d\n", parent_index);
+			//printf("emplacement(h=%d)=%d\n", h, eh);
 			for (int r : views::iota(0, nr_input_rectangles) | views::filter([&](int r){return emplacement(r)==r;}))
 			{
-				printf("emplacement(r=%d)=%d\n", r, r);
+				//printf("emplacement(r=%d)=%d\n", r, r);
 				
-				auto rg = ranges::equal_range(logical_edges, Edge{.from=r,.to=-1});
+				auto rg = ranges::equal_range(logical_edges, r, ranges::less {}, &Edge::from);
 				for (Edge const& le : rg)
 				{
-					printf("le={.from=%d, .to=%d}\n", le.from, le.to);
+					//printf("le={.from=%d, .to=%d}\n", le.from, le.to);
 					Edge te = {.from=emplacement(le.from), .to=emplacement(le.to)};
 					Edge moved_te = {.from=emplacement(h), .to=emplacement(le.to)};
-					printf("te={.from=%d, .to=%d}\n", te.from, te.to);
-					printf("moved_te={.from=%d, .to=%d}\n", moved_te.from, moved_te.to);				
+					//printf("te={.from=%d, .to=%d}\n", te.from, te.to);
+					//printf("moved_te={.from=%d, .to=%d}\n", moved_te.from, moved_te.to);	
+
 					bool test = ranges::binary_search(topological_edges, moved_te, [](const Edge& e1, const Edge& e2){
 						return (e1.from != e2.from) ? e1.from < e2.from : e1.to < e2.to;
 					});
-					printf(test ? "true\n" : "false\n");
+					//printf(test ? "true\n" : "false\n");
 					if (test)
 					{
 						int index = decision_tree.size();
+						
+						if (index > 1000*100)
+							break;
 						
 						const DecisionTreeNode n = {
 							.index = index,
@@ -323,7 +323,7 @@ vector<DecisionTreeNode> compute_decision_tree(int nr_input_rectangles, int nr_e
 							.i_emplacement_destination = moved_te.from
 						};
 
-						printf("{.index=%d, .parent_index=%d, .i_emplacement_source=%d, .i_emplacement_destination=%d}\n", index, n.parent_index, n.i_emplacement_source, n.i_emplacement_destination);
+						//printf("{.index=%d, .parent_index=%d, .i_emplacement_source=%d, .i_emplacement_destination=%d}\n", index, n.parent_index, n.i_emplacement_source, n.i_emplacement_destination);
 
 						decision_tree.push_back(n);
 						
@@ -349,7 +349,72 @@ int main()
 		vector<DecisionTreeNode> decision_tree = compute_decision_tree(nr_input_rectangles, nr_emplacements, logical_edges, topological_edges);
 		
 		bool bOk = expected_decision_tree == decision_tree;
+		
+		int best_idx=-1;
+		int best_result=0;
+		
+		for (int idx=-1; idx < (int)decision_tree.size(); idx++)
+		{
+			vector<int> chemin;
+			for (int index=idx; index != -1; index = decision_tree[index].parent_index)
+			{
+				chemin.push_back(index);
+			}
 
+			auto emplacement = [&](int i){
+				int ei=i;
+				for (int idx : chemin | views::reverse)
+				{
+					const auto& [index, parent_index, i_emplacement_source, i_emplacement_destination] = decision_tree[idx];
+					if (i_emplacement_source==ei)
+						ei = i_emplacement_destination;
+					else if (i_emplacement_destination==ei)
+						ei = i_emplacement_source;
+				}
+				return ei;
+			};
+
+			vector<Edge> topo_edges, inter;
+			for (const Edge& le : logical_edges)
+			{
+				Edge te={emplacement(le.from),emplacement(le.to)};
+				topo_edges.push_back(te);
+			}
+			ranges::sort(topo_edges, [](const Edge& e1, const Edge& e2){
+						return (e1.from != e2.from) ? e1.from < e2.from : e1.to < e2.to;
+					});
+					
+			ranges::set_intersection(topo_edges, topological_edges, back_inserter(inter), [](const Edge& e1, const Edge& e2){
+						return (e1.from != e2.from) ? e1.from < e2.from : e1.to < e2.to;
+					});
+/*					
+			printf("idx=%d\n", idx);
+			printf("topo_edges:\n");
+			for (const Edge& te : topo_edges)
+				printf("{.fom=%d, .to=%d}\n", te.from, te.to);
+			printf("inter:\n");
+			for (const Edge& te : inter)
+				printf("{.fom=%d, .to=%d}\n", te.from, te.to);			
+*/					
+			if (inter.size() > best_result)
+			{
+				best_idx = idx;
+				best_result = inter.size();
+			}
+		}
+
+		printf("\nbest_idx=%d\n", best_idx);
+		printf("best_result=%d\n", best_result);
+
+		for (int idx = best_idx; idx != -1; idx = decision_tree[idx].parent_index)
+		{
+			const DecisionTreeNode& n = decision_tree[idx];
+			int depth=0;
+			for (int index=n.parent_index; index!=-1; index=decision_tree[index].parent_index)
+				depth++;
+			printf("%.*s{.index=%d, .parent_index=%d, .i_emplacement_source=%d, .i_emplacement_destination=%d},\n", depth, "\t\t\t\t\t\t\t\t\t\t", n.index, n.parent_index, n.i_emplacement_source, n.i_emplacement_destination);
+		}
+/*		
 		for (const DecisionTreeNode& n : decision_tree)
 		{
 			int depth=0;
@@ -357,6 +422,7 @@ int main()
 				depth++;
 			printf("%.*s{.index=%d, .parent_index=%d, .i_emplacement_source=%d, .i_emplacement_destination=%d},\n", depth, "\t\t\t\t\t\t\t\t\t\t", n.index, n.parent_index, n.i_emplacement_source, n.i_emplacement_destination);
 		}
+*/
 	}
 	return 0;
 }
