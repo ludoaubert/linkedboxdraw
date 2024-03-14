33277988a6935151be18c8908367f7baaa91090f
Ludovic Aubert <ludo.aubert@gmail.com>
2024-03-14 11:59:17 +0100

    refactoring final step
    context.links = compute_links(selectedContextIndex);
    
    this call instead of duplicate code.

--- diagload.js
+++ diagload.js
@@ -77,53 +77,6 @@ function data2contexts(mydata) {
 	
 	for (const [selectedContextIndex, context] of mycontexts.contexts.entries())
 		context.links = compute_links(selectedContextIndex);
-/*	
-	mycontexts.contexts = mycontexts.contexts.map(
-		({frame, translatedBoxes}) => {
-			const {left,right,top,bottom} = frame;
-			const sframe = [left, right, top, bottom]
-							.map(i => hex(i,4))
-							.join('');
-			console.log(sframe);
-
-			const translations = translatedBoxes
-							.map(({id,translation})=>[translation.x,translation.y])
-							.flat()
-							.map(i => hex(i,3))
-							.join('');
-			console.log(translations);
-
-			const ids = translatedBoxes.map(tB => tB.id);
-			const rectdim_ = translatedBoxes
-							.map(({id}) => rectdim[id])
-							.join('');
-			console.log(rectdim_);
-			console.assert(rectdim_.size == translations.size);
-			console.log(links);
-			const links_ = links.filter(lk => lk.from != lk.to)
-						.filter(lk => lk.category != "TR2")
-						.map(lk => ({from:lk.from, to:lk.to}))
-						.filter(lk => ids.indexOf(lk.from) != -1 && ids.indexOf(lk.to) != -1)
-						.map(lk => [ids.indexOf(lk.from), ids.indexOf(lk.to)])
-						.map(lk => JSON.stringify(lk))
-						.filter(function(lk, pos, self){
-								return self.indexOf(lk) == pos;}
-							) //removing duplicates
-						.map(lk => JSON.parse(lk))
-						.flat()
-						.map(i => hex(i,2))
-						.join('');
-			console.log(links_);
-			const json2 = bombix(rectdim_, translations, sframe, links_);
-			console.log(json2);
-			const polylines = JSON.parse(json2);
-			const polylines2 = polylines.map(({polyline,from,to})=>({polyline, from:ids[from], to:ids[to]}));
-			console.log(polylines2);
-
-			return {frame, translatedBoxes, links:polylines2};
-		}
-	);
-*/
 	
 	return mycontexts;
 }
